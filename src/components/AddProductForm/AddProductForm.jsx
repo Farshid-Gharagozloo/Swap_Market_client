2864d974bd0fc0a6ebd9d00c4e2fdb314151a56e
Farshid-Gharagozloo <g.farshid.ac@gmail.com>
2023-06-23 16:15:43 -0400

    adds the structure of the add product

--- src/components/AddProductForm/AddProductForm.jsx
+++ src/components/AddProductForm/AddProductForm.jsx
@@ -12,6 +12,7 @@ export default function AddProductForm ({onSubmit}){
         description: "",
         price: "",
         interchangeable: "",
+        image: "",
         exchangeable_items: []
     });
     const [itemError, setItemError] = useState({
@@ -22,8 +23,7 @@ export default function AddProductForm ({onSubmit}){
         price: false,
         interchangeable: false,
         exchangeable_items: false       
-    });    
-
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -36,49 +36,65 @@ export default function AddProductForm ({onSubmit}){
             return;
         }
 
-        onSubmit(newItem);
+        let fd = new FormData();
+        fd.append("name", e.target.name.value);
+        fd.append("user_id", 2);
+        fd.append("category", e.target.category.value);
+        fd.append("description", e.target.description.value);
+        fd.append("price", e.target.price.value);
+        fd.append("interchangeable", e.target.interchangeable.value);
+        fd.append("exchangeable_items", []);
+        fd.append("image", e.target.image.files[0]);
+
+        onSubmit(fd);
     };
 
     return (
-        <form action="" className="add-product" onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
-            <div className="add-product__inputs">
-                <Input
-                    placeholder="product Name"
-                    name="name"
-                    hasError={itemError.name}
-                    onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
-                />
-                <Input
-                    placeholder="Category"
-                    name="category"
-                    hasError={itemError.category}
-                    onChange={(e) => setNewItem({ ...newItem, category: e.target.value })}
-                />
-                <Input
-                    placeholder="Description About Item"
-                    name="description"
-                    hasError={itemError.description}
-                    onChange={(e) => setNewItem({ ...newItem, description: e.target.value })}
-                />
-                <Input
-                    placeholder="Price"
-                    name="price"
-                    hasError={itemError.price}
-                    onChange={(e) => setNewItem({ ...newItem, price: Number( e.target.value )})}
-                />
-                <Input
-                    placeholder="Interchangeable"
-                    name="interchangeable"
-                    hasError={itemError.interchangeable}
-                    onChange={(e) => setNewItem({ ...newItem, interchangeable: e.target.value })}
-                />
-
+        <>
+            {/* <form action="http://localhost:8080/upload" placeholder="upload"
+                className="upload" method="POST" encType="multipart/form-data">
                 <input type="file" name="image"/>
-            </div>
-            <div className="add-product__actions">
-                <Button variant='cancel' />
-                <Button type='submit'>Add Product</Button>
-            </div>
-        </form>
+                <input type="submit"/>
+            </form> */}
+            <form className="add-product" onSubmit={handleSubmit} >
+                <div className="add-product__inputs">
+                    <Input
+                        placeholder="product Name"
+                        name="name"
+                        hasError={itemError.name}
+                        onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+                    />
+                    <Input
+                        placeholder="Category"
+                        name="category"
+                        hasError={itemError.category}
+                        onChange={(e) => setNewItem({ ...newItem, category: e.target.value })}
+                    />
+                    <Input
+                        placeholder="Description About Item"
+                        name="description"
+                        hasError={itemError.description}
+                        onChange={(e) => setNewItem({ ...newItem, description: e.target.value })}
+                    />
+                    <Input
+                        placeholder="Price"
+                        name="price"
+                        hasError={itemError.price}
+                        onChange={(e) => setNewItem({ ...newItem, price: Number( e.target.value )})}
+                    />
+                    <Input
+                        placeholder="Interchangeable"
+                        name="interchangeable"
+                        hasError={itemError.interchangeable}
+                        onChange={(e) => setNewItem({ ...newItem, interchangeable: e.target.value })}
+                    />
+                    <input type="file" name="image" accept="image/*" />
+                </div>
+                <div className="add-product__actions">
+                    <Button variant='cancel' />
+                    <Button type='submit'>Add Product</Button>
+                </div>
+            </form>
+        </>
     );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 
-export const DEFAULT_API_KEY = process.env.REACT_APP_API_KEY || 'ThisIsAFallbackApiKey';
+// export const DEFAULT_API_KEY = process.env.REACT_APP_API_KEY || 'ThisIsAFallbackApiKey';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
-axios.defaults.params = {
-  api_key: DEFAULT_API_KEY,
-};
+// axios.defaults.params = {
+//   api_key: DEFAULT_API_KEY,
+// };
 
 const userToken = { headers: {
   Authorization: `Bearer ${sessionStorage.authToken}`,
 }, };
 
+const uploadImg ={headers: {
+  "Content-Type": "multipart/form-data",
+  // Authorization: `Bearer ${sessionStorage.authToken}`,
+},};
+
 export const addUser = (userInformation) => axios.post('/profile/signup',userInformation);
 
 export const getToken = (userPass) => axios.post('/profile/login', userPass);
 
-export const addNewProduct = (newItem) => axios.post('/product/add', newItem);
+export const addNewProduct = (newItem) => axios.post('/product/add', newItem, uploadImg);
 
 export const getUser = (id) => axios.get(`/profile/${id}`,userToken);
 
