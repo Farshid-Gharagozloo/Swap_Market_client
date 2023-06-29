ff008700eca892964f993c692cee9bc126324ae0
Farshid-Gharagozloo <g.farshid.ac@gmail.com>
2023-06-29 10:25:54 -0400

    final version of the phase 1

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -25,7 +25,7 @@ export default function ProductDetails ({productInfo, id}){
             .catch((error) => {
                 return error.console.log(error);
             });
-    }, []);
+    }, [id]);
 
     console.log(categoryList);
 
